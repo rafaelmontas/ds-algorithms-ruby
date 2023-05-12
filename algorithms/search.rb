8b1dd95bc9ff899df985ab6bd6b910b598428afa
Rafael Montas <rafaelmontas1@gmail.com>
2023-05-12 07:42:53 -0400

    Add lower bound implemntation of binary search algorithms

--- algorithms/search.rb
+++ algorithms/search.rb
@@ -1,6 +1,42 @@
 
 module Search
-  # Binary Search: This search returns the index of an item in log(n)
+  # Minimum/lower bound implementation of Binary Search.
+  # Returns the index of the first element that meets the given condition
+  # (block given) in log(n) time provided the array is sorted, but this
+  # is not checked. Returns nil if no element meets the condition.
+  def self.bsearch(array)
+    left, right = 0, array.length
+
+    while left < right
+      mid = (left + right) / 2
+
+      if yield(array[mid])
+        right = mid
+      else
+        left = mid + 1
+      end
+    end
+
+    return left == array.length ? nil : left
+  end
+
+  # Minimum/lower bound implementation of Binary Search as described at
+  # method #bsearch, but makes use of recursion instead of using a loop.
+  def self.bsearch_rec(array, left, right, &block)
+    if left >= right
+      return left == array.length ? nil : left
+    else
+      mid = (left + right) / 2
+      
+      if yield(array[mid])
+        self.bsearch_rec(array, left, mid, &block)
+      else
+        self.bsearch_rec(array, mid + 1, right, &block)
+      end
+    end
+  end
+  
+  # Standard Binary Search: This search returns the index of an item in log(n)
   # time provided the array is sorted. The mehod returns nil if item
   # is not found.
   def self.binary_search(array, item)
@@ -23,8 +59,9 @@ module Search
     return nil
   end
 
-  # This method implements Binary Search using recursion
-  # The mehod returns nil if item is not found
+  # Standard Binary Search as described at method #binary_search but
+  # using recursion. Returns the index of an item in log(n) time provided
+  # the array is sorted. The mehod returns nil if item is not found.
   def self.rec_binary_search(array, item, min, max)
     if max < min
       return nil
